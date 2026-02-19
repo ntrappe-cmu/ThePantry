1efbde47349dc5c7add7c4a25f4e9f0683f75e44
ntrappe-cmu <ntrappe@andrew.cmu.edu>
2026-02-19 17:02:28 -0500

    add tests for menubar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,33 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
-import MenuBar from './components/menu-bar';
-import DonationCard from './components/donation-card';
+import MenuBar from './components/MenuBar/menu-bar';
 
 function App() {
+  const [currentView, setCurrentView] = useState('home');
+
+  useEffect(() => {
+    /**
+     * Listen for menuBarSwitch events from MenuBar component
+     * Updates the current view when a menu item is clicked
+     */
+    const handleMenuSwitch = (event) => {
+      const { view } = event.detail;
+      setCurrentView(view);
+      console.log(`Switched to view: ${view}`);
+    };
+
+    document.addEventListener('menuBarSwitch', handleMenuSwitch);
+
+    // Cleanup: remove event listener on component unmount
+    return () => {
+      document.removeEventListener('menuBarSwitch', handleMenuSwitch);
+    };
+  }, []);
 
   return (
     <>
       <div className='scrollable-container'>
-        <DonationCard />
-        <DonationCard />
-        <DonationCard />
-        <DonationCard />
-        <DonationCard />
-        <DonationCard />
-        <DonationCard />
-        <DonationCard />
-        <DonationCard />
+        {/* Cards go here */}
       </div>
       <MenuBar />
     </>
---
+++ frontend/tests/jest/placeholder.js
@@ -0,0 +1,3 @@
+test('basic adder', () => {
+  expect(1 + 2).toEqual(3);
+});
