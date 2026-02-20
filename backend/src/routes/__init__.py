da09274f448113f06438362f38a1b56a23d0d566
Trey Tuscai <ttuscai22@icloud.com>
2026-02-20 14:18:20 -0500

    feat: added blueprints __init__ for package import

--- backend/src/routes/__init__.py
+++ backend/src/routes/__init__.py
@@ -1,2 +1,6 @@
 from .donation_routes import donation_bp
-__all__ = ["donation_bp"]
+from .hold_routes import hold_bp
+from .history_routes import history_bp
+from .user_routes import user_bp
+
+__all__ = ["donation_bp", "hold_bp", "history_bp", "user_bp"]
